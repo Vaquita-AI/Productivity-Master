3b505d90be02eae14cb2d288620920647c13f8b7
Vaquita-AI <54004380+Vaquita-AI@users.noreply.github.com>
2024-04-27 10:42:18 +0200

    Update database.py
    added Pomodoro Timer

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 
 def batch_rename(directory, pattern, replace_with):
     """
@@ -20,3 +22,32 @@ def batch_rename(directory, pattern, replace_with):
 # Example of usage:
 # This will rename all files in the 'sample_directory' that have 'old' in their name to include 'new' instead.
 batch_rename('sample_directory', 'old', 'new')
+
+def pomodoro_timer(work_time=25, break_time=5):
+    """
+    A simple Pomodoro timer that alternates between work time and break time.
+
+    :param work_time: The duration of the work period in minutes.
+    :param break_time: The duration of the break period in minutes.
+    """
+
+    def start_timer(duration):
+        end_time = time.time() + duration * 60
+        while time.time() < end_time:
+            mins, secs = divmod(end_time - time.time(), 60)
+            timer = '{:02d}:{:02d}'.format(int(mins), int(secs))
+            print(f"\rTime left: {timer}", end="")
+            time.sleep(1)
+        print()
+
+    while True:
+        print("Work time! Get focused and start working.")
+        start_timer(work_time)
+        print("Time for a break! Relax for a bit.")
+        start_timer(break_time)
+        cont = input("Press 'n' if you want to stop the Pomodoro timer, or just hit Enter to continue: ")
+        if cont.lower() == 'n':
+            break
+
+# Run the Pomodoro timer
+pomodoro_timer()
